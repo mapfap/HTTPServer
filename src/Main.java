ed5ceeaaadd70a343d574f6900e7b971975ec617
mapfap <mapkabb@gmail.com>
2014-04-08 23:58:53 +0700

    Add logger.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Main {
 	public static void main(String[] args) {
+		
 		new Server(5000, new Process() {
 
 			@Override
@@ -7,6 +12,18 @@ public class Main {
 				return "Thank you.";
 			}
 		
-		});
+		}, getLogger());	
+	}
+	
+	public static Logger getLogger() {
+		Logger logger = Logger.getLogger("log");  
+	    try { 
+	    	FileHandler fh = new FileHandler("server.log");  
+	    	fh.setFormatter(new SimpleFormatter());
+	        logger.addHandler(fh);
+	    } catch (Exception e) {  
+	        e.printStackTrace();
+	    }
+	    return logger;
 	}
 }
--- src/Server.java
+++ src/Server.java
@@ -5,17 +5,18 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URLDecoder;
+import java.util.Calendar;
+import java.util.logging.Logger;
 
 public class Server {
 
-	public Server(int port, Process process) {
+	public Server(int port, Process process, Logger logger) {
 		try {
-			@SuppressWarnings("resource")
 			ServerSocket server = new ServerSocket(port);
 			System.out.println("Server is running at port " + port);
 			while (true) {
 				Socket client = server.accept();
-				(new ServerThread(client, process)).start();
+				(new ServerThread(client, process, logger)).start();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -28,10 +29,16 @@ class ServerThread extends Thread {
 
 	private Socket client;
 	private Process process;
+	private Logger logger;
 
-	public ServerThread(Socket client, Process process) {
+	public ServerThread(Socket client, Process process, Logger logger) {
 		this.client = client;
 		this.process = process;
+		this.logger = logger;
+	}
+	
+	public static String getTimestamp() {
+		return Calendar.getInstance().getTime().toString();
 	}
 
 	@Override
@@ -40,7 +47,7 @@ class ServerThread extends Thread {
 			
 			String incomingIP = client.getInetAddress().toString();
 			int incomingPort = client.getPort();
-			System.out.println(">> " + incomingIP + ":" + incomingPort);
+			logger.info("Connection from: " + incomingIP + ":" + incomingPort);
 			InputStream in = client.getInputStream();
 			OutputStream out = client.getOutputStream();
 
@@ -61,22 +68,24 @@ class ServerThread extends Thread {
 				sb.append((char) reader.read());
 			}
 			
-			String data = sb.toString().split("\r\n\r\n")[1];
-			data = URLDecoder.decode(data, "UTF-8");
+//			String data = sb.toString().split("\r\n\r\n")[1];
+//			data = URLDecoder.decode(data, "UTF-8");
 //			System.out.println(data);
-			System.out.println(sb);
+			logger.info(sb.toString());
 
 			String response = "HTTP/1.1 200 OK\r\nServer: Mapfap1.0\r\nContent-Type: text/html\r\nAccess-Control-Allow-Origin: *\r\n\r\n";
-			response += process.run(data) + "\r\n";
+//			response += process.run(data) + "\r\n";
+			response += process.run("") + "\r\n";
 
 			byte[] bytes = response.getBytes();
 			out.write(bytes);
 			out.flush();
 			in.close();
 			out.close();
-			System.out.println(">> Connection closed.");
+			logger.info("Connection closed.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
 }
